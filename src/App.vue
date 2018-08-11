44500cb74286af9be0d8526bb14ce16114500d0d
杨嘉乐 <782196556@qq.com>
2018-08-11 14:57:04 +0800

    第一次

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <!-- 这里就是vue的视图层----即 MVVM 中的 V -->
+  <div id="app">
+    <h1>{{ msg }}</h1>
+    <marquee></marquee>
+  </div>
+</template>
+<script>
+// import Vue from './vue'
+import marquee from './components/marquee'
+//  new出来的 vm 对象就是MVVM 中的 vm 调度者
+  export default {
+    name: 'app',
+    //data 是vue的页面的数据层-----即 M，专门保存每个页面数据
+    data() {
+      return {
+        msg: '这是一个vue 的数据'
+      }
+    }, 
+    components: {
+      marquee
+    }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ src/components/marquee.vue
@@ -0,0 +1,48 @@
+<template>
+    <!-- 跑马灯效果 -->
+    <div>
+        <input value="浪起来" type="button" @click="lang"> 
+        <input value="低调" type="button" @click="stop">
+
+        <div class="div">{{ msg }}</div>
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: "猥琐发育！别浪！",
+      timer: null
+    };
+  },
+  mounted (){
+      this.lang();
+  },
+  methods: {
+    lang() {
+        if(this.timer != null) return ;
+      this.timer = setInterval(() => {
+        var start = this.msg.substring(0, 1);
+        var end = this.msg.substring(1);
+        this.msg = end + start;
+      }, 400);
+    },
+    stop() {
+        clearInterval(this.timer);
+        this.timer = null;
+    }
+  }
+};
+</script>
+
+<style>
+input {
+  margin-right: 20px;
+}
+.div {
+  margin-top: 20px;
+  margin: 30px;
+}
+</style>
+
